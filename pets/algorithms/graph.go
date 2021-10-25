9323243c3b99976818de9ac64cd75cb760a8750a
JackSlide <loisoj@gmail.com>
2021-10-25 15:10:46 +0300

    fmt

--- pets/algorithms/graph.go
+++ pets/algorithms/graph.go
@@ -1,64 +1,64 @@
 //Алгоритм поиска в ширину по графам
 package main
 
-import(
-  "fmt"
-  "container/list"
+import (
+	"container/list"
+	"fmt"
 )
 
 func isManager(name string) bool {
-  //Тупо для теста, если имя заканчивается на м - то менеджер
-  if name[len(name)-1:] == "m" {
-    return true
-  } else {
-    return false
-  }
+	//Тупо для теста, если имя заканчивается на м - то менеджер
+	if name[len(name)-1:] == "m" {
+		return true
+	} else {
+		return false
+	}
 }
 
 func findManagerInGraph(graph map[string][]string) string {
 
-  // инициализируем очередь
-  queue := list.New()
+	// инициализируем очередь
+	queue := list.New()
 
-  //Добавляем в очередь первичный графф, связи 1 уровня
-  for _, value := range graph["you"] {
-    queue.PushBack(value)
-  }
+	//Добавляем в очередь первичный графф, связи 1 уровня
+	for _, value := range graph["you"] {
+		queue.PushBack(value)
+	}
 
-  //Работаем с очередью
-  for queue.Len() > 0 {
-    e := queue.Front() // Первый элемент
-    name := e.Value.(string)
+	//Работаем с очередью
+	for queue.Len() > 0 {
+		e := queue.Front() // Первый элемент
+		name := e.Value.(string)
 
-    if isManager(name) {
-       return name
-       break
-    } else {
-      if len(graph[name]) > 0 {
-        for _, value := range graph[name] {
-          queue.PushBack(value)
-        }
-      }
-    }
-    queue.Remove(e)
-  }
+		if isManager(name) {
+			return name
+			break
+		} else {
+			if len(graph[name]) > 0 {
+				for _, value := range graph[name] {
+					queue.PushBack(value)
+				}
+			}
+		}
+		queue.Remove(e)
+	}
 
-  return "Нету менеджеров"
+	return "Нету менеджеров"
 
 }
 
 func main() {
 
-  graph := map[string][]string{}
-  graph["you"] = []string{"alice","bob","claire"}
-  graph["bob"] = []string{"anuj","peggy"}
-  graph["alice"] = []string{"peggy"}
-  graph["claire"] = []string{"thom", "jonny"}
-  graph["anuj"] = []string{}
-  graph["peggy"] = []string{}
-  graph["thom"] = []string{}
-  graph["jonny"] = []string{}
+	graph := map[string][]string{}
+	graph["you"] = []string{"alice", "bob", "claire"}
+	graph["bob"] = []string{"anuj", "peggy"}
+	graph["alice"] = []string{"peggy"}
+	graph["claire"] = []string{"thom", "jonny"}
+	graph["anuj"] = []string{}
+	graph["peggy"] = []string{}
+	graph["thom"] = []string{}
+	graph["jonny"] = []string{}
 
-  fmt.Println(findManagerInGraph(graph))
+	fmt.Println(findManagerInGraph(graph))
 
 }
