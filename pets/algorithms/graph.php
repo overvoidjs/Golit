92ef77c44d7b2314d59a8a72b87a169bc29806c3
overvoidjs <loisoj@gmail.com>
2021-10-27 13:21:17 +0300

    Graph upd

--- pets/algorithms/graph.php
+++ pets/algorithms/graph.php
@@ -5,11 +5,11 @@ class Queue {
   public $data = [];
 
   // Получаем длину очереди
-  public function getLength() {
+  public function getLength():int {
     return count($this->data);
   }
   // Определяем, пуста ли очередь
-  public function isEmpty() {
+  public function isEmpty():bool {
     return $this->getLength() === 0;
   }
 
@@ -19,22 +19,65 @@ class Queue {
   }
 
   //Получаем первый элемент очереди и удаляем его из очереди
-  public function getFront() {
-    return array_shift($this->data);
+  public function getFront():string {
+    return (string)array_shift($this->data);
   }
 
-  public function getAll(){
+  //Вернуть все
+  public function getAll():array {
     return $this->data;
   }
 
 }
 
 
-$q = new Queue();
+function isManager(string $name):bool {
+  //Тупо для теста, если имя заканчивается на м - то менеджер
+  if( substr($name, -1) == "m" ){
+    return true;
+  } else {
+    return false;
+  }
+}
 
-$q->pushBack('a');
-$q->pushBack('b');
-$q->pushBack('c');
 
-var_dump($q->getFront());
-var_dump($q->getAll());
+function findManagerInGraph(array $graph):string {
+  // инициализируем очередь
+  $q = new Queue();
+
+  //Добавляем в очередь первичный графф, связи 1 уровня
+  foreach ($graph["you"] as $key => $value) {
+    $q->pushBack($value);
+  }
+
+  //Работаем с очередью
+  while ( !$q->isEmpty() ) {
+    $name = $q->getFront();
+
+    if( isManager($name) ){
+      return $name;
+    } else {
+      if( count($graph[$name]) > 0 ){
+        foreach ($graph[$name] as $key => $value) {
+          $q->pushBack($value);
+        }
+      }
+    }
+
+  }
+
+  return "Нету менеджеров";
+}
+
+
+$graph = [];
+$graph["you"] = ["alice","bob","claire"];
+$graph["bob"] = ["anuj","peggy"];
+$graph["alice"] = ["peggy"];
+$graph["claire"] = ["thom", "jonny"];
+$graph["anuj"] = [];
+$graph["peggy"] = [];
+$graph["thom"] = [];
+$graph["jonny"] = [];
+
+var_dump(findManagerInGraph($graph));
