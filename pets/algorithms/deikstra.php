64cd1dd38da81cc9c7360db0d2d7d00524d534ca
overvoidjs <loisoj@gmail.com>
2021-10-27 18:53:37 +0300

    What the hell is going on?!?

---
+++ pets/algorithms/deikstra.php
@@ -0,0 +1,67 @@
+<?php
+
+//Граф соседей
+$graph["start"] = [];
+$graph["start"]["a"] = 6;
+$graph["start"]["b"] = 2;
+$graph["a"] = [];
+$graph["a"]["fin"] = 1;
+$graph["b"] = [];
+$graph["b"]["a"] = 3;
+$graph["b"]["fin"] = 5;
+$graph["fin"] = [];
+
+//Таблица стоимостей
+$costs = [];
+$costs["a"] = 6;
+$costs["b"] = 2;
+$costs["fin"] = INF;
+
+//Таблица родителей
+$parents = [];
+$parents["a"] = "start";
+$parents["b"] = "start";
+$parents["in"] = null;
+
+//Таблица уже обработанный узлов графа
+$processed = [];
+// Переменная есть
+// var_dump($processed);
+// die();
+
+//Функция поиска узла с наименьшей стоимостью среди необработанных
+function findLowestCostNode($costs){
+  $lowest_cost = INF;
+  $lowest_cost_node = null;
+  foreach ($costs as $key => $cost) {
+    //А тут ее вдруг нет
+    var_dump($processed);
+    die();
+    if( $cost < $lowest_cost && !in_array($key, $processed) ){
+      $lowest_cost = $cost;
+      $lowest_cost_node = $key;
+    }
+  }
+  return $lowest_cost_node;
+}
+
+$node = findLowestCostNode($costs);
+
+while ( !is_null($node) ) {
+  $cost = $costs[$node];
+  $neighbors = $graph[$node];
+  foreach (array_keys($neighbors) as $key => $n) {
+    $new_cost = $cost + $neighbors[$n];
+    if( $costs[$n] > $new_cost ){
+      $costs[$n] = $new_cost;
+      $parents[$n] = $node;
+    }
+  }
+  $processed[] = $node;
+  // А тут опять есть
+  // var_dump($processed);
+  // die();
+  $node = findLowestCostNode($costs);
+}
+
+var_dump( $costs );
